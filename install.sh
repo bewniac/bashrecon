56e591e7a2cd7533cb63b7d1a90b2014a5aaa3fd
bewniac <benjamin@hackad.nu>
2021-10-23 11:29:10 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/zsh
+
+# Variables
+BINDIR="/home/bewniac/projects/bashrecon/bin"
+CWD="/home/bewniac/projects/bashrecon"
+APT_DEPS="git make build-essential ruby ruby-dev unzip nmap jq chromium-browser"
+
+# clean
+if [ $1 = "--clean" ]
+then 
+    echo "Cleaning up"
+    rm -rf bin
+    rm -rf nuclei-templates
+    rm -f /home/bewniac/go/bin/amass
+    rm -f /home/bewniac/go/bin/crobat
+    rm -f /home/bewniac/go/bin/subfinder
+    rm -f /home/bewniac/go/bin/httpx
+    rm -f /home/bewniac/go/bin/nuclei
+    sudo rm -f /usr/local/bin/massdns
+
+    echo "Optionally, remove installed dependencies:"
+    echo "sudo apt remove $APT_DEPS && sudo rm -rf /usr/local/go"
+    exit 0 
+fi
+
+
+# Create bin directory
+if [ -d $BINDIR ];
+then
+    echo "Already installed everything. To reinstall use --clean first"
+    exit 0
+else
+    mkdir bin
+fi
+
+# recon deps
+sudo apt update && sudo apt install $APT_DEPS
+wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+rm go1.17.2.linux-amd64.tar.gz
+
+# Crobat
+go get github.com/cgboal/sonarsearch/cmd/crobat
+cp `which crobat` $BINDIR/crobat
+
+# Nuclei
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+git clone https://github.com/projectdiscovery/nuclei-templates.git
+cp `which nuclei` $BINDIR/
+
+# HttpX
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+cp `which httpx` $BINDIR/
+
+# Subfinder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+cp `which subfinder` $BINDIR/
+
+# WPScan
+sudo gem install wpscan
+cp `which wpscan` $BINDIR/
+
+# MassDNS
+git clone https://github.com/blechschmidt/massdns.git
+cd massdns && make && sudo make install 
+cp `which massdns` $BINDIR/
+cd $CWD && sudo rm -r massdns
+
+# Amass
+wget https://github.com/OWASP/Amass/releases/download/v3.14.1/amass_linux_amd64.zip
+cp amass_linux_amd64/amass bin/
+rm -r amass_linux_amd64 amass_linux_amd64.zip
+
+# Nmap
+cp `which nmap` $BINDIR/nmap
+
+# Aquatone
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
+unzip -q -o -d bin/ aquatone_linux_amd64_1.7.0.zip -x "README.md" "LICENSE.txt"
+rm -f aquatone_linux_amd64_1.7.0.zip
+
+# DNSRecon
+git clone https://github.com/darkoperator/dnsrecon.git
+cd dnsrecon && python3 -m pip install -r requirements.txt && cd $CWD
+ln -s $CWD/dnsrecon/dnsrecon.py $BINDIR/dnsrecon
---
+++ recon.sh
@@ -0,0 +1,85 @@
+#!/bin/zsh
+
+function usage () {
+    echo "Usage: $1 example.com"
+}
+
+
+if [ -z "$1" ];
+then
+    usage $0
+    exit 0
+fi
+
+TARGET=$1
+RESULTDIR="result/$TARGET/$(date +%y%m%d_%H%M)"
+mkdir -p $RESULTDIR
+
+# Clean resolver list
+wget https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -O resolvers.txt
+
+# DNSrecon
+./bin/dnsrecon -d $TARGET -j $RESULTDIR/dnsrecon.json
+
+# DNSrecon for new tlds on target
+./bin/dnsrecon -t tld -d $TARGET -j $RESULTDIR/tlds.json
+# TLDs from DNSrecon
+jq -cr '.[1:] | .[] | .[].name' $RESULTDIR/tlds.json | sort | uniq >> tlds.txt
+
+# Amass
+./bin/amass enum -nolocaldb -d $TARGET -json $RESULTDIR/amass.json &
+
+# Subfinder
+./bin/subfinder -silent -d $TARGET -oJ -o $RESULTDIR/subfinder.json &
+
+# Crobat 
+./bin/crobat -s $TARGET | tee $RESULTDIR/crobat.txt &
+
+# Wait until everything is done
+wait
+
+echo "Creating subdomains.txt and cleaning it up"
+jq -r .name >> $RESULTDIR/subdomains.txt $RESULTDIR/amass.json
+jq -r .host >> $RESULTDIR/subdomains.txt $RESULTDIR/subfinder.json
+cat $RESULTDIR/crobat.txt >> $RESULTDIR/subdomains.txt
+sort $RESULTDIR/subdomains.txt | uniq >> $RESULTDIR/tmp.txt
+mv $RESULTDIR/tmp.txt $RESULTDIR/subdomains.txt
+
+./bin/massdns -o J -r resolvers.txt $RESULTDIR/subdomains.txt -w $RESULTDIR/massdns.json & 
+./bin/httpx -silent -l $RESULTDIR/subdomains.txt -o $RESULTDIR/httpx.json -json & 
+
+# Wait until everything is done
+wait 
+
+# Fix massdns output
+jq -cr  '.data.answers[] | select(.type=="A") | .data' 2>/dev/null $RESULTDIR/massdns.json > $RESULTDIR/ipv4.txt
+jq -cr  '.data.answers[] | select(.type=="AAAA") | .data' 2>/dev/null $RESULTDIR/massdns.json > $RESULTDIR/ipv6.txt
+jq -cr  '.data.answers[] | select(.type=="CNAME") | .data' 2>/dev/null $RESULTDIR/massdns.json > $RESULTDIR/cname.txt
+
+cat $RESULTDIR/httpx.json | jq -r .url >> $RESULTDIR/webservers.txt
+
+# Nuclei 
+./bin/nuclei -t / -l $RESULTDIR/webservers.txt -o $RESULTDIR/nuclei.json -json -silent -irr -r resolvers.txt & 
+./bin/nmap -v --top-ports 10000 -g 53 -Pn -sV -iL $RESULTDIR/ipv4.txt -oA $RESULTDIR/nmap & 
+
+# Wait until everything is done
+wait
+
+# TODO: Cleanup & parse nuclei result
+jq -c '."template-id", .info.severity, .info.name, ."curl-command"' $RESULTDIR/nuclei.json
+
+# Get wordpress nuclei results
+jq -cr 'select(."matcher-name"=="wordpress") | .host' $RESULTDIR/nuclei.json 2>/dev/null | grep http | cut -d ":" -f 1-2 | sort | uniq >> wordpress.txt
+
+# WPScan
+for url in `cat $RESULTDIR/wordpress.txt`;
+do
+    FILENAME=`echo $url | cut -d "/" -f 3`
+    wpscan --rua -o $RESULTDIR/$FILENAME -e ap --disable-tls-checks --ignore-main-redirect --url $url
+done
+
+# Auquatone on nmap xml
+cat $RESULTDIR/nmap.xml | ./bin/aquatone -silent -ports xlarge -nmap -out $RESULTDIR/aquatone/ &
+
+# Always end with a wait
+wait 
